8686ee977bdfa45e1252e7d60f08243503ac2f9b
marcoGullich <marco.gullich@gmail.com>
2025-11-03 19:36:37 -0300

    Setup inicial
    Setup inicial

---
+++ src/test/java/com/marcoGullich/pmanager/PManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoGullich.pmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
