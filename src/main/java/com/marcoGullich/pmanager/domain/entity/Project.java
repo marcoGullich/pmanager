cb1eeae01a0bbae171773aef79bf8e35df071af8
marcoGullich <marco.gullich@gmail.com>
2025-11-25 21:13:21 -0300

    Criação da Entity Project
    Criação da Entity Project

---
+++ src/main/java/com/marcoGullich/pmanager/domain/entity/Project.java
@@ -0,0 +1,43 @@
+package com.marcoGullich.pmanager.domain.entity;
+
+import com.marcoGullich.pmanager.domain.model.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.UUID;
+
+@Entity
+@Table(name = "project")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = UUID)
+    @Column(name = "id", nullable = false, length = 36)
+    //Anotação é opcional.. JPA consegue realizar a criação das colunas.
+    private String id;
+
+    @Column(name = "name", nullable = false, length = 80)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 150)
+    private String description;
+
+    @Column(name = "initial_date", nullable = false)
+    private LocalDate initialDate;
+
+    @Column(name = "final_date", nullable = false)
+    private LocalDate finalDate;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING) // String pega a descrição. Já o ORDINAL pega o número de cada tipo(0(PENDING), 1, 2)
+    private ProjectStatus status;
+}
---
+++ src/main/java/com/marcoGullich/pmanager/domain/model/ProjectStatus.java
@@ -0,0 +1,6 @@
+package com.marcoGullich.pmanager.domain.model;
+
+public enum ProjectStatus {
+
+    PENDING, IN_PROGRESS, FINISHED
+}
